819ce817334554cbfdd7a3045aeffe9bbf92acd4
ali <alicryptovolley@gmail.com>
2022-06-28 18:54:37 +0430

    places and distanitions finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
 const $ = document;
 const menuOpener = $.querySelector(".menu-opener");
 const collapseMenu = $.querySelector(".collapse-menu");
+const placeIcons = $.querySelectorAll(".place__icon");
+// const likeIcon = $.querySelector(".place__icon-svg");
+
+placeIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    icon.classList.toggle("place__icon--like");
+  });
+});
 
 function showCollapseMenu() {
   collapseMenu.classList.toggle("collapse-menu--show");
